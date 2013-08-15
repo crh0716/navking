928fd6743dc0dbc53f51d109f92d15f3baac6208
Arthur Chen <crh0716@gmail.com>
2013-08-15 22:46:05 +0800

    url test

--- js/index.js
+++ js/index.js
@@ -77,10 +77,12 @@ function check() {
     if (scanner) {
       scanner.scan(
         function(result) {
-          alert('We got a barcode\n' +
+          //navigator.app.loadUrl(result.text, { openExternal:true });
+          window.open(result.text, '_blank');
+          /*alert('We got a barcode\n' +
                 'Result: ' + result.text + '\n' +
                 'Format: ' + result.format + '\n' +
-                'Cancelled: ' + result.cancelled);
+                'Cancelled: ' + result.cancelled);*/
         },
         function(error) {
           alert('Scanning failed: ' + error);
